b253c4017a134d14d3b8c0159c01f58e97be4270
hqweay <hqweay@gmail.com>
2026-01-18 19:22:39 +0800

    upgrade#1.0.9 fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 import externalGlobals from "rollup-plugin-external-globals";
 import livereload from "rollup-plugin-livereload";
-import path from "path";
+import zipPack from "vite-plugin-zip-pack";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const isDev = mode === "development";
-
+  const devDistDir = "/Users/hqweay/Documents/orca/plugins/orca-hwqeay-go/dist";
   return {
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
-        "#": path.resolve(__dirname, "."),
+        "@": resolve(__dirname, "src"),
+        "#": resolve(__dirname, "."),
       },
     },
     define: {
@@ -22,9 +23,8 @@ export default defineConfig(({ command, mode }) => {
     },
     build: {
       emptyOutDir: true, // 强制清空输出目录
-      outDir: isDev
-        ? "/Users/hqweay/Documents/orca/plugins/orca-hwqeay-go/dist"
-        : "./dist",
+      outDir: isDev ? devDistDir : "./dist",
+      minify: !isDev,
       lib: {
         entry: "src/main.tsx",
         fileName: "index",
@@ -32,6 +32,15 @@ export default defineConfig(({ command, mode }) => {
       },
       rollupOptions: {
         external: ["react", "valtio"],
+        plugins: !isDev
+          ? [
+              zipPack({
+                inDir: "./dist",
+                outDir: "./",
+                outFileName: "package.zip",
+              }),
+            ]
+          : [],
       },
       // 开发模式下启用监听
       watch: isDev
