1b570b169cc27b81b3cf38fee332cafc265dc9f3
hqweay <hqweay@gmail.com>
2026-04-30 02:10:26 +0800

    refactor: simplify child component embedding logic in translation handler

--- src/translations/parts/embedChildren.ts
+++ src/translations/parts/embedChildren.ts
@@ -1,10 +1,7 @@
 export default {
   "embed-children": "块嵌入子项",
   "embed-children.description":
-    "启用后可支持特定标签的块在嵌入为镜像块时展示其子块并附带特殊样式。",
-  "lets-embed-children": "支持块嵌入子项",
-  "lets-embed-children.description":
-    "启用后可支持特定标签的块在嵌入为镜像块时展示其子块并附带特殊样式。",
+    "一个块如果打上配置的标签，它的子块中的镜像块将只展示子项。达到 Roam Research 中的 Block Embed Children 效果。",
   "Embed Children Settings": "块嵌入子项设置",
   "Tag Name": "标签名",
   "The tag name to trigger the embed children style": "触发该样式的标签名称",
