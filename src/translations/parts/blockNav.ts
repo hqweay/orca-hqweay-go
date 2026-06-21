edd7ab11bff8d45dcdf00a386f3886fce8523e73
hqweay <hqweay@gmail.com>
2026-06-22 02:29:39 +0800

    fix: resolve broken translation keys in block navigation component

--- src/translations/parts/blockNav.ts
+++ src/translations/parts/blockNav.ts
@@ -1,6 +1,7 @@
 export default {
   "block-nav": "大纲导航",
-  "block-nav.description": "全局侧边栏大纲树，支持全文检索与左右键分离的下钻交互",
+  "block-nav.description":
+    "全局侧边栏大纲树，支持全文检索与左右键分离的下钻交互",
   "block-nav.no-block": "暂无数据",
   "block-nav.no-children": "无子块",
   "block-nav.expand-all": "展开全部",
@@ -9,5 +10,7 @@ export default {
   "block-nav.delete-selected": "删除",
   "block-nav.drop-to-add": "释放以添加为子块",
   "block-nav.sidebarPosition": "侧边栏位置",
-  "block-nav.sidebarPositionDesc": "大纲导航默认打开的位置。右键点击按钮可以在另一侧打开。",
+  "block-nav.sidebarPositionDesc":
+    "大纲导航默认打开的位置。右键点击按钮可以在另一侧打开。",
+  "block-nav.settings": "大纲导航设置",
 };
